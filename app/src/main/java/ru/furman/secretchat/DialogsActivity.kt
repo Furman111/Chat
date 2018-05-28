c12efc9fa3455ecacc5f2260287857da9b34e8bc
furmansd <furmansd@altarix.ru>
2018-05-28 18:06:58 +0400

    Initial commit

---
+++ app/src/main/java/ru/furman/secretchat/DialogsActivity.kt
@@ -0,0 +1,13 @@
+package ru.furman.secretchat
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DialogsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dialogs)
+    }
+
+}
